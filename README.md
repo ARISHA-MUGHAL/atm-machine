5ba7829522272b233cb2cab44e25f5ae7c90ff30
ARISHA_MUGHAL <160270089+ARISHA-MUGHAL@users.noreply.github.com>
2024-04-16 05:18:52 +0500

    created atm-machine

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # atm-machine
-This repository contains a simple calculator built using TypeScript, Node.js, and Inquirer. The calculator allows users to perform basic arithmetic operations through a command-line interface.
+## Overview
+This project is a simple ATM (Automated Teller Machine) simulation implemented in TypeScript using the Inquirer library. It allows users to perform basic banking operations such as withdrawing cash, depositing cash, fast cash withdrawal with predefined amounts, and checking balance.
+
+## Features
+- **Withdrawal**: Users can withdraw cash from their accounts.
+- **Deposit**: Users can deposit cash into their accounts.
+- **Fast Cash**: Users can quickly withdraw predefined amounts.
+- **Check Balance**: Users can check their account balance.
+
+
+## Usage
+1. Upon running the application, the user will be prompted to choose from available options: Withdraw, Deposit, Fast Cash, or Check Balance.
+2. Follow the on-screen prompts to complete the desired transaction.
+3. After completing the transaction, the user can choose to perform another operation or exit the application.
+
+## Dependencies
+- [TypeScript] =TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.
+- [Inquirer] =For interactive command-line user interfaces.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Author
+ARISHA MUGHAL
---
+++ main.js
@@ -0,0 +1,61 @@
+import inquirer from "inquirer";
+let myBalance = 50000;
+let myPin = 3638;
+let atmPin = await inquirer.prompt({
+    name: "pinCode",
+    type: "number",
+    message: "Please type your pin code ",
+});
+if (atmPin.pinCode === myPin) {
+    console.log("correct pin code");
+    let action = await inquirer.prompt({
+        name: "command",
+        message: "what command you want to give ",
+        type: "list",
+        choices: ["withdraw", "check balance", "deposit", "fast cash"],
+    });
+    if (action.command === "withdraw") {
+        let amountAns = await inquirer.prompt({
+            name: "amount",
+            message: "How much amount you want to withdraw ? ",
+            type: "number",
+        });
+        if (amountAns.amount > myBalance) {
+            console.log("you dont have this much money");
+        }
+        else {
+            myBalance -= amountAns.amount;
+            console.log("your remaining balance is : " + myBalance);
+        }
+    }
+    else if (action.command === "check balance") {
+        console.log(`your current balance is ${myBalance} `);
+    }
+    else if (action.command === "deposit") {
+        let amountInfo = await inquirer.prompt({
+            name: "depositamount",
+            message: "How much amount you want to deposit ? ",
+            type: "number",
+        });
+        myBalance += amountInfo.depositamount;
+        console.log("Now your balance is : " + myBalance);
+    }
+    else if (action.command === "fast cash") {
+        let fastCash = await inquirer.prompt({
+            name: "preDefinedAmount",
+            message: "Choose how much amount you want to withdraw",
+            type: "list",
+            choices: ["1000", "5000", "10000", "15000", "20000", "25000", "30000", "40000", "50000"]
+        });
+        if (fastCash.preDefinedAmount > myBalance) {
+            console.log("you dont have this much money");
+        }
+        else {
+            myBalance -= fastCash.preDefinedAmount;
+            console.log("your remaining balance is : " + myBalance);
+        }
+    }
+}
+else {
+    console.log("incorrect pin code");
+}
